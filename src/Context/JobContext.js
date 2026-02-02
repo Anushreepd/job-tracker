3bab3d1870e98fd84d28e9c13592042af4828a21
Anushreepd <anushreepd14@gmail.com>
2026-02-02 06:23:10 +0530

    localStorage useage

--- src/Context/JobContext.js
+++ src/Context/JobContext.js
@@ -1,10 +1,29 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import { jobReducer } from "./JobReducer";
 
 export const JobContext = createContext();
 
 function JobProvider({ children }) {
-  const [jobs, dispatch] = useReducer(jobReducer, []);
+
+  const initialValue = () => {
+    const storedData = localStorage.getItem("jobs");
+
+    if (!storedData || storedData === "undefined") {
+      return [];
+    }
+
+    try {
+      return JSON.parse(storedData);
+    } catch {
+      return [];
+    }
+  };
+
+  const [jobs, dispatch] = useReducer(jobReducer, [], initialValue);
+
+  useEffect(() => {
+    localStorage.setItem("jobs", JSON.stringify(jobs));
+  }, [jobs]);
 
   return (
     <JobContext.Provider value={{ jobs, dispatch }}>
